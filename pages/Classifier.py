23ff848bc95d3d7d33ce8a64557a8eb8bb97bb55
Kavibharathi S <149556133+kavi11662@users.noreply.github.com>
2025-11-20 14:09:12 +0530

    Update Classifier.py

---
+++ pages/Classifier.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from PIL import Image
+from utils import global_css, load_model, classify_image
+
+st.set_page_config(page_title="Classifier", page_icon="🧪")
+global_css()
+
+st.markdown('<h1 class="title">EcoSort AI Classifier</h1>', unsafe_allow_html=True)
+
+model = load_model()
+
+option = st.selectbox("Choose Input Type:", ["Upload Image", "Live Camera"])
+
+# Upload
+if option == "Upload Image":
+    file = st.file_uploader("Upload Waste Image", type=["jpg","jpeg","png"])
+    if file:
+        img = Image.open(file)
+        st.image(img, use_container_width=True)
+
+        cls, conf, tip = classify_image(img, model)
+
+        st.markdown(f"""
+        <div class="result-card">
+            <p class="predicted">{cls.upper()}</p>
+            <p class="confidence">Confidence: {conf:.2f}%</p>
+            <p class="tip">{tip}</p>
+        </div>
+        """, unsafe_allow_html=True)
+
+# Camera
+else:
+    cam = st.camera_input("Capture Image")
+    if cam:
+        img = Image.open(cam)
+        st.image(img, use_container_width=True)
+
+        cls, conf, tip = classify_image(img, model)
+
+        st.markdown(f"""
+        <div class="result-card">
+            <p class="predicted">{cls.upper()}</p>
+            <p class="confidence">Confidence: {conf:.2f}%</p>
+            <p class="tip">{tip}</p>
+        </div>
+        """, unsafe_allow_html=True)
