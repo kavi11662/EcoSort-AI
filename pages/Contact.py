db76d42bf1188adbe183909abeb077a741e0fc32
Kavibharathi S <149556133+kavi11662@users.noreply.github.com>
2025-11-20 14:09:36 +0530

    Update Contact.py

---
+++ pages/Contact.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from utils import global_css
+
+st.set_page_config(page_title="Contact", page_icon="📞")
+global_css()
+
+st.markdown('<h1 class="title">Contact</h1>', unsafe_allow_html=True)
+
+st.write("""
+### Developer  
+**Kavibharathi S**
+
+### Internship  
+AICTE – Shell – Edunet Green Skills Internship 🌍
+
+### Project  
+EcoSort AI — Smart Waste Classification System
+""")
