7aba8115337d070d8d6571b9a15348b53975b8d3
Kavibharathi S <149556133+kavi11662@users.noreply.github.com>
2025-11-20 12:16:32 +0530

    Update and rename Classification.py to Classifier.py

--- Classifier.py
+++ Classifier.py
@@ -1,14 +1,10 @@
 import streamlit as st
 from PIL import Image
-from utils import load_model, classify_image, ECO_TIPS, CLASS_NAMES
-from utils import navbar
-from utils import global_css
-
-
+from utils import navbar, global_css, load_model, classify_image
 
 st.set_page_config(page_title="EcoSort Classifier", page_icon="♻")
-global_css()
 
+global_css()
 navbar()
 
 st.markdown('<h1 class="title">EcoSort AI Classifier</h1>', unsafe_allow_html=True)
@@ -17,34 +13,36 @@ model = load_model()
 
 option = st.selectbox("Choose Input Type:", ["Upload Image", "Live Camera Feed"])
 
+# ------- Upload Image -------
 if option == "Upload Image":
-    file = st.file_uploader("Upload Waste Image", type=["jpg","jpeg","png"])
+    file = st.file_uploader("Upload Waste Image", type=["jpg", "jpeg", "png"])
     if file:
         img = Image.open(file)
         st.image(img, use_container_width=True)
 
-        cls, conf = classify_image(model, img)
+        cls, conf, tip = classify_image(img, model)
 
         st.markdown(f"""
         <div class="result-card">
             <p class="predicted">{cls.upper()}</p>
             <p class="confidence">Confidence: {conf:.2f}%</p>
-            <p class="tip">{ECO_TIPS[cls]}</p>
+            <p class="tip">{tip}</p>
         </div>
         """, unsafe_allow_html=True)
 
+# ------- Live Camera -------
 elif option == "Live Camera Feed":
     cam = st.camera_input("Capture Image")
     if cam:
         img = Image.open(cam)
         st.image(img, use_container_width=True)
 
-        cls, conf = classify_image(model, img)
+        cls, conf, tip = classify_image(img, model)
 
         st.markdown(f"""
         <div class="result-card">
             <p class="predicted">{cls.upper()}</p>
             <p class="confidence">Confidence: {conf:.2f}%</p>
-            <p class="tip">{ECO_TIPS[cls]}</p>
+            <p class="tip">{tip}</p>
         </div>
         """, unsafe_allow_html=True)
