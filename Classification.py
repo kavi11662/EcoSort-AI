67d23d30de37fb906ee7d190248b29c5c789779e
Kavibharathi S <149556133+kavi11662@users.noreply.github.com>
2025-11-20 11:58:55 +0530

    Implement EcoSort AI Classifier with image upload

---
+++ Classification.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from PIL import Image
+from utils import load_model, classify_image, ECO_TIPS, CLASS_NAMES
+from utils import navbar
+
+st.set_page_config(page_title="EcoSort Classifier", page_icon="♻")
+
+navbar()
+
+st.markdown('<h1 class="title">EcoSort AI Classifier</h1>', unsafe_allow_html=True)
+
+model = load_model()
+
+option = st.selectbox("Choose Input Type:", ["Upload Image", "Live Camera Feed"])
+
+if option == "Upload Image":
+    file = st.file_uploader("Upload Waste Image", type=["jpg","jpeg","png"])
+    if file:
+        img = Image.open(file)
+        st.image(img, use_container_width=True)
+
+        cls, conf = classify_image(model, img)
+
+        st.markdown(f"""
+        <div class="result-card">
+            <p class="predicted">{cls.upper()}</p>
+            <p class="confidence">Confidence: {conf:.2f}%</p>
+            <p class="tip">{ECO_TIPS[cls]}</p>
+        </div>
+        """, unsafe_allow_html=True)
+
+elif option == "Live Camera Feed":
+    cam = st.camera_input("Capture Image")
+    if cam:
+        img = Image.open(cam)
+        st.image(img, use_container_width=True)
+
+        cls, conf = classify_image(model, img)
+
+        st.markdown(f"""
+        <div class="result-card">
+            <p class="predicted">{cls.upper()}</p>
+            <p class="confidence">Confidence: {conf:.2f}%</p>
+            <p class="tip">{ECO_TIPS[cls]}</p>
+        </div>
+        """, unsafe_allow_html=True)
