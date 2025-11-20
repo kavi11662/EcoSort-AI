d9f53591ff4b9dc5323cdefb98f0ce73df2320f3
Kavibharathi S <149556133+kavi11662@users.noreply.github.com>
2025-11-20 12:15:36 +0530

    Update Features.py

--- Features.py
+++ Features.py
@@ -1,11 +1,8 @@
 import streamlit as st
-from utils import navbar
-from utils import global_css
-
-
-
+from utils import navbar, global_css
 
 st.set_page_config(page_title="Features", page_icon="♻")
+
 global_css()
 navbar()
 
@@ -13,8 +10,9 @@ st.markdown('<h1 class="title">Features</h1>', unsafe_allow_html=True)
 
 st.write("""
 ### ✅ 10-Class Waste Detection  
-### ✅ Upload Image or Live Camera Capture  
-### ✅ Eco-friendly Disposal Tips  
+### ✅ Upload Image or Live Camera  
+### ✅ Instant Eco-Friendly Tips  
+### ✅ HuggingFace Model Integration  
 ### ✅ Neon Dark Professional UI  
-### ✅ HuggingFace-powered Deep Learning Model  
+### ✅ Multi-Page Website Navigation  
 """)
