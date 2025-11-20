7ab67fb0055b571f78dc08081d27ba64f06c2384
Kavibharathi S <149556133+kavi11662@users.noreply.github.com>
2025-11-20 13:59:39 +0530

    Update About.py

--- About.py
+++ About.py
@@ -1,17 +1,17 @@
-import streamlit as st
-from utils import navbar, global_css
+# import streamlit as st
+# from utils import navbar, global_css
 
-st.set_page_config(page_title="About EcoSort", page_icon="♻")
+# st.set_page_config(page_title="About EcoSort", page_icon="♻")
 
-global_css()
-navbar()
+# global_css()
+# navbar()
 
-st.markdown('<h1 class="title">About EcoSort AI</h1>', unsafe_allow_html=True)
+# st.markdown('<h1 class="title">About EcoSort AI</h1>', unsafe_allow_html=True)
 
-st.write("""
-EcoSort AI helps solve the problem of improper waste segregation by using AI  
-to instantly classify 10 types of waste and provide the correct disposal method.
+# st.write("""
+# EcoSort AI helps solve the problem of improper waste segregation by using AI  
+# to instantly classify 10 types of waste and provide the correct disposal method.
 
-The goal is to support citizens and communities in promoting cleaner environments  
-and reducing landfill burden.
-""")
+# The goal is to support citizens and communities in promoting cleaner environments  
+# and reducing landfill burden.
+# """)
